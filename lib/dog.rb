4241b9af8aa49a56e9814d1f1d5dff61bb2a1237
Janice Darikho <janicedarikho@gmail.com>
2018-05-12 11:20:14 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def name=(dog_name)
+    @dog_name = dog_name
+  end
+  
+  def name
+    @name = name
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+  end
+end
